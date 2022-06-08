f1cee37a886118286368bdfffb2b85df2a47be0a
Vivyen Vu <pokeviv@gmail.com>
2022-06-08 13:43:46 -0400

    scene change for onClickMainAddPartBtn

--- src/controller/mainFormController.java
+++ src/controller/mainFormController.java
@@ -1,9 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,8 +27,13 @@ public class mainFormController implements Initializable {
         System.out.println("I am initialized!");
     }
 
-    public void onClickMainAddPartBtn(ActionEvent actionEvent) {
-        System.out.println("Adding part");
+    public void onClickMainAddPartBtn(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/addPart.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 500, 1080);
+        stage.setTitle("Add Part");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void onClickMainModPartBtn(ActionEvent actionEvent) {
