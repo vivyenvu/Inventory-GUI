7949c0ed25fcf4ba9f265cd3a3996cd62220f986
Vivyen Vu <pokeviv@gmail.com>
2022-06-08 13:54:11 -0400

    action for exit button in addPartController)

--- src/controller/addPartController.java
+++ src/controller/addPartController.java
@@ -1,4 +1,28 @@
 package controller;
 
-public class addPartController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class addPartController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void toMain(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/mainForm.fxml"));
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 500, 1080);
+        stage.setTitle("Back to Main Screen");
+        stage.show();
+    }
 }
