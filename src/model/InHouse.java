e8868f26f27cdeb74270ec557dc9ffd0016a2885
Vivyen Vu <pokeviv@gmail.com>
2022-06-10 08:50:52 -0400

    InHouse

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -1,4 +1,18 @@
 package model;
 
-public class InHouse {
+public class InHouse extends Part{
+    private int machineID;
+
+    public InHouse(int partID, String partName, double partPrice, int partStock, int partMin, int partMax, int machineID, boolean madeInHouse) {
+        super(partID, partName, partPrice, partStock, partMin, partMax, madeInHouse);
+        this.machineID = machineID;
+    }
+
+    public void setMachineID(int machineID) {
+        this.machineID = machineID;
+    }
+
+    public int getMachineID() {
+        return machineID;
+    }
 }
