cbcb05476639cb946b3f1a07e2cbb9712c17f999
Vivyen Vu <pokeviv@gmail.com>
2022-06-10 08:54:03 -0400

    Outsource

--- src/model/Outsourced.java
+++ src/model/Outsourced.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Outsourced {
+public class Outsourced extends Part{
+    private String companyName;
+
+    public Outsourced(int partID, String partName, double partPrice, int partStock, int partMin, int partMax, boolean partMadeInHouse, String companyName) {
+        super(partID, partName, partPrice, partStock, partMin, partMax, partMadeInHouse);
+        this.companyName = companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
 }
