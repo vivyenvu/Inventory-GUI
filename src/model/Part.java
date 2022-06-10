c1ee5e510572c49b848e3470f5824feac0d08948
Vivyen Vu <pokeviv@gmail.com>
2022-06-10 07:28:20 -0400

    Part abstract class and getteres

--- src/model/Part.java
+++ src/model/Part.java
@@ -1,4 +1,50 @@
 package model;
 
-public class Part {
+public abstract class Part {
+    private int partID;
+    private String partName;
+    private double partPrice;
+    private int partStock;
+    private int partMin;
+    private int partMax;
+    boolean partMadeInHouse;
+
+    public Part(int partID, String partName, double partPrice, int partStock, int partMin, int partMax, boolean partMadeInHouse) {
+        this.partID = partID;
+        this.partName = partName;
+        this.partPrice = partPrice;
+        this.partStock = partStock;
+        this.partMin = partMin;
+        this.partMax = partMax;
+        this.partMadeInHouse = partMadeInHouse;
+    }
+
+    //getters
+     public int getID() {
+        return partID;
+     }
+
+     public String getName() {
+        return partName;
+     }
+
+     public double getPrice() {
+        return partPrice;
+     }
+
+     public int getStock() {
+        return partStock;
+     }
+
+     public int getMin() {
+        return partMin;
+     }
+
+     public int getMax() {
+        return partMax;
+     }
+
+     public boolean isPartMadeInHouse() {
+        return partMadeInHouse;
+     }
 }
