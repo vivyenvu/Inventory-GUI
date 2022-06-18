5be806e61071980257299b18797f82d5d5544999
Vivyen Vu <pokeviv@gmail.com>
2022-06-18 10:51:05 -0400

    corrected data

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        InHouse OPB = new InHouse(1, "OPB", 200, 15, 0, int 50, 1234, true);
+        InHouse OPB = new InHouse(1, "OPB", 200, 15, 0, 50, 1234, true);
         InHouse DB = new InHouse (2, "Deadlift Bar", 300, 10, 0, 40, 2468, true);
         Outsourced plate = new Outsourced (3, "Eleiko plates", 180, 44, 0, 100, false);
         Outsourced platform = new Outsourced (4, "Wood platform", 500, 12, 0, 20, false);
