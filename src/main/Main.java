2a2cc3ac1714481ddb797fcc7a82161ff53876d2
Vivyen Vu <pokeviv@gmail.com>
2022-06-18 10:45:41 -0400

    test data

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.InHouse;
 
 public class Main extends Application {
     @Override
@@ -16,6 +17,9 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        InHouse OPB = new InHouse(1, "OPB", 200, 15, 0, int 50, 1234, true);
+        InHouse DB = new InHouse (2, "Deadlift Bar", 300, 10, 0, 40, 2468, true);
+
         launch(args);
     }
 }
