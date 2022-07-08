f9e2aff798c3cd795b7a9bc9fc87fe54c02c2f84
Vivyen Vu <pokeviv@gmail.com>
2022-07-08 16:21:35 -0400

    Para javadocs

--- src/model/Part.java
+++ src/model/Part.java
@@ -1,7 +1,12 @@
 package model;
 
-import javafx.scene.control.Alert;
+/**
+ * Supplied class Part.java
+ */
 
+/**
+ * @author Vivyen Vu
+ */
 public abstract class Part {
     private int partID;
     private String partName;
@@ -11,6 +16,16 @@ public abstract class Part {
     private int partMax;
     boolean madeInHouse;
 
+    /**
+     * Constructor for a Part
+     * @param partID part identification number
+     * @param partName name of part
+     * @param partPrice price of part
+     * @param partStock inventory level of part
+     * @param partMin miniumum inventory level
+     * @param partMax maximum inventory level
+     * @param madeInHouse tells whether part was made in house or outsourced
+     */
     public Part(int partID, String partName, double partPrice, int partStock, int partMin, int partMax, boolean madeInHouse) {
         this.partID = partID;
         this.partName = partName;
@@ -22,59 +37,103 @@ public abstract class Part {
     }
 
     //Getters
+
+    /**
+     * @return part identification number
+     */
      public int getPartID() {
         return partID;
      }
 
+    /**
+     * @return part's name
+     */
      public String getPartName() {
         return partName;
      }
 
+    /**
+     * @return part's price
+     */
      public double getPartPrice() {
         return partPrice;
      }
 
+    /**
+     * @return part's inventory level
+     */
      public int getPartStock() {
         return partStock;
      }
 
+    /**
+     * @return part's minimum inventory level
+     */
      public int getPartMin() {
         return partMin;
      }
 
+    /**
+     * @return part's maximum inventory level
+     */
      public int getPartMax() {
         return partMax;
      }
 
+    /**
+     * @return whether or not the part was made in house
+     */
      public boolean isMadeInHouse() {
         return madeInHouse;
      }
 
      //Setters
+
+    /**
+     * @param partID the part's identification number
+     */
     public void setPartID (int partID){
         this.partID = partID;
     }
 
+    /**
+     * @param partName the part's name
+     */
     public void setPartName(String partName) {
         this.partName = partName;
     }
 
+    /**
+     * @param partPrice the part's price
+     */
     public void setPartPrice(double partPrice) {
         this.partPrice = partPrice;
     }
 
+    /**
+     * @param partStock the part's inventory level
+     */
     public void setPartStock (int partStock) {
         this.partStock = partStock;
     }
 
+    /**
+     * @param partMin the part's minimum inventory level
+     */
     public void setPartMin (int partMin) {
         this.partMin = partMin;
     }
 
+    /**
+     * @param partMax the part's maximum inventory level
+     */
     public void setPartMax (int partMax) {
         this.partMax = partMax;
     }
-    
+
+    /**
+     * @param madeInHouse whether or not the part was made in house
+     */
     public void setMadeInHouse (boolean madeInHouse) {
         this.madeInHouse = madeInHouse;
     }
@@ -82,6 +141,19 @@ public abstract class Part {
     /**
      * FUTURE ENHANCEMENT: Incorporate validation in setters to improve readability
      * and modularity of code.
+     */
+
+    /**
+     * This method takes the data input in the text fields and validates them. Eg. name has to be a string and
+     * cannot be empty, price must be a double, etc.
+     * @param name name of part
+     * @param price price of part
+     * @param stock stock of part
+     * @param min minimum inventory level
+     * @param max maximum inventory level
+     * @param machOrComp entered data that may either be the machineID or the companyName
+     * @param madeInHouse whether or not the part was made in house
+     * @return invalid which is a string including data validation errors
      */
     public static String validPart(String name,String price, String stock, String min, String max, String machOrComp, boolean madeInHouse) {
         double priced = 1.00;
