38a54db28af829e159a41dcb1d1d64f6d7e2665f
Vivyen Vu <pokeviv@gmail.com>
2022-07-08 16:31:15 -0400

    Product javadocs

--- src/model/Product.java
+++ src/model/Product.java
@@ -6,6 +6,9 @@ import javafx.collections.ObservableList;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Product type with List of associated parts
+ */
 public class Product {
     private int prodID;
     private String prodName;
@@ -15,6 +18,15 @@ public class Product {
     private int prodMax;
     private ObservableList<Part> associatedParts;
 
+    /**
+     * Constructor for Product object
+     * @param prodID product identification number
+     * @param prodName name of product
+     * @param prodPrice price of product
+     * @param prodStock inventory level of product
+     * @param prodMin minimum inventory level
+     * @param prodMax maximum inventory level
+     */
     public Product(int prodID, String prodName, double prodPrice, int prodStock, int prodMin, int prodMax) {
         this.prodID = prodID;
         this.prodName = prodName;
@@ -25,73 +37,136 @@ public class Product {
         List<Part> partsList = new ArrayList<Part>();
         this.associatedParts = FXCollections.observableList(partsList);
     }
+
     //Getters
+
+    /**
+     * @return prodID, product identification number
+     */
     public int getProdID() {
         return prodID;
     }
 
+    /**
+     * @return prodName, name of product
+     */
     public String getProdName() {
         return prodName;
     }
 
+    /**
+     * @return prodPrice, price of product
+     */
     public double getProdPrice() {
         return prodPrice;
     }
 
+    /**
+     * @return prodStock, inventory level of product
+     */
     public int getProdStock() {
         return prodStock;
     }
 
+    /**
+     * @return prodMin, minimum inventory level
+     */
     public int getProdMin() {
         return prodMin;
     }
 
+    /**
+     * @return prodMax, maximum inventory level
+     */
     public int getProdMax() {
         return prodMax;
     }
 
+    /**
+     * @return associatedParts, List of parts associated with this product
+     */
     public ObservableList<Part> getAllAssociatedParts() {
         return associatedParts;
     }
     //Setters
 
+    /**
+     * @param prodID, product identification number
+     */
     public void setProdID (int prodID) {
         this.prodID = prodID;
     }
 
+    /**
+     * @param prodName, name of product
+     */
     public void setProdName(String prodName) {
         this.prodName = prodName;
     }
 
+    /**
+     * @param prodPrice, price of product
+     */
     public void setProdPrice(double prodPrice) {
         this.prodPrice = prodPrice;
     }
 
+    /**
+     * @param prodStock, inventory level of product
+     */
     public void setProdStock(int prodStock) {
         this.prodStock = prodStock;
     }
 
+    /**
+     * @param prodMin, minimum inventory level
+     */
     public void setProdMin(int prodMin){
         this.prodMin = prodMin;
     }
 
+    /**
+     * @param prodMax, maximum inventory level
+     */
     public void setProdMax (int prodMax) {
         this.prodMax = prodMax;
     }
 
+    /**
+     * @param parts, List of parts associated with product
+     */
     public void setProdParts (ObservableList<Part> parts) {
         this.associatedParts = parts;
     }
 
+    /**
+     * Adds one part to the List of that product's associated parts
+     * @param part, part to be added to associated parts list
+     */
     public void addAssociatedPart(Part part) {
         associatedParts.add(part);
     }
 
+    /**
+     * Removes specific part from product's list of associated parts
+     * @param selectedAssociatedPart, part to be removed
+     * @return true when deletion occurs
+     */
     public boolean deleteAssociatedPart(Part selectedAssociatedPart){
         this.associatedParts.remove(selectedAssociatedPart);
         return true;
     }
 
+    /**
+     * This method takes the data input in the text fields and validates them. Eg. name has to be a string and
+     * cannot be empty, price must be a double, etc.
+     * @param name name of product
+     * @param price price of product
+     * @param stock stock of product
+     * @param min minimum inventory level
+     * @param max maximum inventory level
+     * @return invalid which is a string including data validation errors
+     */
     public static String validProd(String name,String price, String stock, String min, String max) {
         double priced = 1.00;
         int stocki = 1;
